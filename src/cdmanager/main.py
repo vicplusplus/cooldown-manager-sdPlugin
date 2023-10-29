a2516145150daf1dc070f0d7a90378c3053aacb7
Victor Martinez <vicplusplus@proton.me>
2023-10-28 23:49:13 -0400

    make main.py more user-friendly

--- src/cdmanager/main.py
+++ src/cdmanager/main.py
@@ -8,11 +8,13 @@ ctx_to_event = {}
 
 
 async def handle(websocket):
+    print("New connection established.")
     async for message in websocket:
         await register_cooldown_hotkey(json.loads(message), websocket)
 
 
 async def main():
+    print("WebSocket server started. Awaiting connections on 'localhost:8765'...")
     async with serve(handle, "localhost", 8765):
         await asyncio.Future()  # run forever
 
@@ -23,31 +25,33 @@ async def register_cooldown_hotkey(data, ws):
     try:
         add_cooldown_hotkey(data["context"], data["payload"]["settings"]["keycode"], ws)
     except KeyError:
-        print("Missing key")
+        print("Error: Received message is missing a required key.")
 
 
 async def update_client(ws, ctx):
     try:
         await ws.send(ctx)
-        print("fired", ctx)
+        # print("fired", ctx)  # commented out to reduce excessive logging
     except:
+        print(f"Error updating client for context '{ctx}'. Closing connection.")
         await ws.close()
         remove_cooldown_hotkey(ctx)
 
 
 def remove_cooldown_hotkey(ctx):
-    keyboard.remove_hotkey(ctx_to_event[ctx])
+    event, key = ctx_to_event[ctx]
+    keyboard.remove_hotkey(event)
     ctx_to_event.pop(ctx)
-    print("removed", ctx)
+    print(f"Key '{key}' has been unregistered.")
 
 
 def add_cooldown_hotkey(ctx, key, ws):
     try:
         event = keyboard.add_hotkey(key, lambda: asyncio.run(update_client(ws, ctx)))
-        ctx_to_event[ctx] = event
-        print("registered", key)
+        ctx_to_event[ctx] = (event, key)
+        print(f"Key '{key}' has been registered.")
     except ValueError:
-        print("invalid key", key, "(ignoring)")
+        print(f"Error: Invalid key '{key}'. Ignoring.")
 
 
 asyncio.run(main())
